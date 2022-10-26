4a7574f4159581dad5de610875e699d17181cba6
Pedro <pedroagustinrojo@gmail.com>
2022-10-25 23:13:36 -0300

    primera entrega

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -1,10 +1,25 @@
+import CartWidget from "../CartWidget";
+import './styles.css';
+
 const NavBar = () => {
     return (
         <ul>
-            <li><a href="#home">Home</a></li>
-            <li><a href="#news">News</a></li>
-            <li><a href="#contact">Contact</a></li>
-            <li><a href="#about">About</a></li>
+            <li>
+                <a href="#inicio"><h3>Inicio</h3></a>
+            </li>
+            <li>
+                <a href="#novedades"><h3>Novedades</h3></a>
+            </li>
+            <li>
+                <a href="#masVendidos"><h3>Mas Vendidos</h3></a>
+            </li>
+            <li>
+                <a href="#contactanos"><h3>Contactanos</h3></a>
+            </li>
+            <li>
+                <h1>Pa' Jugar</h1>
+            </li>
+            <CartWidget/>
         </ul>
 
     )
